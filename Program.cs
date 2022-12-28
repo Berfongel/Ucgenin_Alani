10683b40ac16f0b5eadf818053f94669c6853538
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 15:16:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+double yukseklik, taban_uzunlugu, alan;
+
+Console.WriteLine("Yükseklik değerini giriniz : ");
+yukseklik = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Taban uzunluk değerini giriniz : ");
+taban_uzunlugu = Convert.ToDouble(Console.ReadLine());
+alan = ((taban_uzunlugu*yukseklik)/2);
+Console.WriteLine("Üçgenin alanı : " + alan);
