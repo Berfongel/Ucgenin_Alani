eeac47b8f130a6542ca7ab90cd606005f7cb2074
Berfongel <121492984+Berfongel@users.noreply.github.com>
2023-01-04 17:01:15 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UcgenAlaniHesaplamaWebApplication.Models;
+
+namespace UcgenAlaniHesaplamaWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(UcgenAlanHesabi ucgenAlanHesabi)
+        {
+            var yukseklik = ucgenAlanHesabi.Yukseklik;
+            var tabanUzunlugu = ucgenAlanHesabi.TabanUzunlugu;
+            var alan=(yukseklik*tabanUzunlugu)/ 2;
+
+            ViewBag.Message = "Üçgenin Alanı : " + alan;
+            return View("ucgenAlanHesabi");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UcgenAlanHesabi.cs
@@ -0,0 +1,10 @@
+﻿namespace UcgenAlaniHesaplamaWebApplication.Models
+{
+    public class UcgenAlanHesabi
+    {
+        public double Yukseklik { get; set; }
+        public double TabanUzunlugu { get; set; }
+
+    }
+}
+
